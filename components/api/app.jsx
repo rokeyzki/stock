acbdbb92aa6f333056007c93413660e227032681
rokeyzki <rokeyzki@foxmail.com>
2016-05-22 13:23:37 +0000

    app@echarts

--- components/api/app.jsx
+++ components/api/app.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import jsonp from 'jsonp';
+import jsonp from 'jsonp'; // jsonp 时，使用jsonp
+
+// import $ from 'jquery'; // curl（带header）时，使用$.ajax
 
 var UserGist = React.createClass({
   getInitialState: function() {
@@ -12,6 +14,7 @@ var UserGist = React.createClass({
   },
 
   componentDidMount: function() {
+    
     jsonp(this.props.source, function (err, data) {
       if (err) throw err;
         console.log(data);
@@ -22,6 +25,21 @@ var UserGist = React.createClass({
           });
         }
     }.bind(this));
+    
+    
+    /*$.ajax({
+        url:this.props.source,
+        headers:{ apikey: '5e12a6f652ce580a2a0108c89fe57c83' },
+        success : function(result){
+            if (this.isMounted()) {
+                console.log(JSON.parse(result));
+                this.setState({
+                    
+                });
+            }
+        }.bind(this)
+    });*/
+    
   },
 
   render: function() {
@@ -33,9 +51,8 @@ var UserGist = React.createClass({
   }
 });
 
-// https://api.github.com/users/rokeyzki/gists
-// http://web.juhe.cn:8080/finance/stock/hk?num=00799&key=60bd29533d9517cd6f91c64ccf558d35
-// http://op.juhe.cn/onebox/stock/query?dtype=&stock=igg&key=1e03e0734081578f97c39c41ba36ff0a
+// jsonp: http://op.juhe.cn/onebox/stock/query?dtype=&stock=igg&key=1e03e0734081578f97c39c41ba36ff0a
+// curl: http://apis.baidu.com/showapi_open_bus/stock_his/sh_stock_his?begin=2015-09-01&end=2015-09-02&code=600004
 ReactDOM.render(
   <UserGist source="http://op.juhe.cn/onebox/stock/query?dtype=&stock=igg&key=1e03e0734081578f97c39c41ba36ff0a" />,
   document.querySelector("#app")
--- entry/index.entry.js
+++ entry/index.entry.js
@@ -1,12 +1,12 @@
 // 兼容IE8的处理，放在入口文件的最前面 npm install --save es5-shim console-polyfill es6-promise fetch-ie8
-require('es5-shim');
-require('es5-shim/es5-sham');
-require('console-polyfill');
-require('es6-promise');
-require('fetch-ie8');
+// require('es5-shim');
+// require('es5-shim/es5-sham');
+// require('console-polyfill');
+// require('es6-promise');
+// require('fetch-ie8');
 
 // app
-require("../components/api/app.jsx");
+require("../components/echarts/app.jsx");
 
 // db
 // require("../model/leanCloud/select.js");
