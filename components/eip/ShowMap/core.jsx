ebec0e50b6b5ca38d20ada2f92bede82c915b2c2
rokeyzki <rokeyzki@foxmail.com>
2016-07-09 10:46:26 +0000

    router

---
+++ components/eip/ShowMap/core.jsx
@@ -0,0 +1,94 @@
+import React from 'react';
+
+import echarts from 'echarts';
+
+const ShowMap = React.createClass({
+  componentDidMount: function() {
+    // ECharts
+    // 获取DOM对象
+    var chart = this.refs.signEcharts;
+    // 基于准备好的dom，初始化echarts实例
+    var myChart = echarts.init(chart);
+
+    // 指定图表的配置项和数据
+    var option = {
+        backgroundColor: '#2c343c',
+    
+        title: {
+            text: 'Customized Pie',
+            left: 'center',
+            top: 20,
+            textStyle: {
+                color: '#ccc'
+            }
+        },
+    
+        tooltip : {
+            trigger: 'item',
+            formatter: "{a} <br/>{b} : {c} ({d}%)"
+        },
+    
+        visualMap: {
+            show: false,
+            min: 80,
+            max: 600,
+            inRange: {
+                colorLightness: [0, 1]
+            }
+        },
+        series : [
+            {
+                name:'访问来源',
+                type:'pie',
+                radius : '55%',
+                center: ['50%', '50%'],
+                data:[
+                    {value:335, name:'直接访问'},
+                    {value:310, name:'邮件营销'},
+                    {value:274, name:'联盟广告'},
+                    {value:235, name:'视频广告'},
+                    {value:400, name:'搜索引擎'}
+                ].sort(function (a, b) { return a.value - b.value}),
+                roseType: 'angle',
+                label: {
+                    normal: {
+                        textStyle: {
+                            color: 'rgba(255, 255, 255, 0.3)'
+                        }
+                    }
+                },
+                labelLine: {
+                    normal: {
+                        lineStyle: {
+                            color: 'rgba(255, 255, 255, 0.3)'
+                        },
+                        smooth: 0.2,
+                        length: 10,
+                        length2: 20
+                    }
+                },
+                itemStyle: {
+                    normal: {
+                        color: '#c23531',
+                        shadowBlur: 200,
+                        shadowColor: 'rgba(0, 0, 0, 0.5)'
+                    }
+                }
+            }
+        ]
+    };
+
+    // 使用刚指定的配置项和数据显示图表。
+    myChart.setOption(option);
+  },
+  
+  render: function() {
+    return (
+      <div>
+        <div ref='signEcharts' style={{width: "100%", height: "400px", margin: "50px 0px"}}></div>
+      </div>
+    );
+  }
+});
+
+export default ShowMap;
